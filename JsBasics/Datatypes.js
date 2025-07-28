b74ecd72afa4f472b3b3eddded4ca89a61c070be
KomalKumawat17 <64780102+KomalKumawat17@users.noreply.github.com>
2025-07-28 11:37:44 +0000

    Datatypes

---
+++ JsBasics/Datatypes.js
@@ -0,0 +1,19 @@
+"use strict" //treat all JS code as newer version
+var notdefinedvalue;
+var bigintvalue = 123446678990111111111111111111111111111111111111111111111111111111111111111111111111111111;
+
+/*
+   number => 2 power 53
+   string
+   boolean
+   symbol => unique
+   bigint
+   undefined
+   null
+
+   object
+*/
+
+console.log('Typeof Null ::; ',typeof(null)); // object
+console.log('Typeof notdefinedvalue ::; ',typeof(notdefinedvalue)); // undefined
+console.log('Typeof bigintvalue ::; ',typeof(bigintvalue));
